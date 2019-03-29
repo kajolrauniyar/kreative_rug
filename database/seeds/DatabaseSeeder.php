c415ac671e614260dd9362d3e6eba204b2805474
Kajol2608 <rauniyarkajol2608@gmail.com>
2019-03-29 15:32:12 +0545

    db seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        // $this->call(LaratrustSeeder::class);
-        $this->call(TableSeeder::class);
+        $this->call(LaratrustSeeder::class);
+        // $this->call(TableSeeder::class);
     }
 }
