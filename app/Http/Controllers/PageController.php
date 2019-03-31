31e82908e556854e6df9c22b90739ecc69b1a452
Kajol2608 <rauniyarkajol2608@gmail.com>
2019-03-31 16:50:40 +0545

    changes

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -162,7 +162,7 @@ class PageController extends Controller
                 }
                 else{
                     $content = new Content;
-                    $content->page_id => $page->id;
+                    $content->page_id = $page->id;
                     $content->sectionTitle = $request->sectionTitle[$i];
                     $content->sectionContent = $request->sectionTitle[$i];
                     $content->save();
