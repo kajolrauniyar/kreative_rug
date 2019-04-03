4b52771e3d398c9dedc5c4a5e15c062b88fabb72
Kajol2608 <rauniyarkajol2608@gmail.com>
2019-04-03 16:36:27 +0545

    edit

--- app/Http/Controllers/FaqController.php
+++ app/Http/Controllers/FaqController.php
@@ -81,12 +81,13 @@ class FaqController extends Controller
      * @param  \App\Faq  $faq
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Faq $faq)
+    public function update(Request $request)
     {
         $this->validate($request,[
             'query' => 'required',
             'answer' => 'required',
         ]);
+        $faq = Faq::find($request->id);
         $faq ->query = $request->query;
         $faq ->answer = $request->answer;
         $faq->save();
