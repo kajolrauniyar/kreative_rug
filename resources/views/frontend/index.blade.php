d08dcc5d3bf61b14b1577dfe8352b76ea068ce49
Kajol2608 <rauniyarkajol2608@gmail.com>
2019-05-07 19:15:29 +0545

    intro grid

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -72,6 +72,34 @@
         </div>        
     </div>
 </section>
+<section class="section-featured">
+        <div class="section-title">
+            <h2 class="heading-secondary">{{$home->section2_title}}</h2>
+            <span class="divide-line"></span>
+        </div>
+        <div class="section-content">
+            <div class="section-content__centered">
+                {{$home->section2_content}}
+            </div>
+        </div>
+        <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slider="sets: true">
+
+            <ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-4@m uk-grid-small">
+                @foreach ($categories as $category)
+                <li>
+                    <img src="{{$category->thumb}}" alt="{{$category->slug}}">
+                    <div class="uk-position-center uk-panel">
+                        <h3><a href="{{ route('frontend.category',$category->slug) }}">{{$category->name}}</a></h3>
+                    </div>
+                </li>
+                @endforeach
+            </ul>
+
+            <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+            <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
+
+        </div>
+    </section>
 
 <!--
 <section class="value-grid">
