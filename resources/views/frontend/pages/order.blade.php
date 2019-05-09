283865951d2c3376bb70cbbd4331bf8a073e15d2
Kajol2608 <rauniyarkajol2608@gmail.com>
2019-05-09 13:59:30 +0545

    refactor

--- resources/views/frontend/pages/order.blade.php
+++ resources/views/frontend/pages/order.blade.php
@@ -2,17 +2,29 @@
 @extends('layouts.frontend') 
 @section('content')
 @include('frontend.partials._page-header')
+@if (isset($page->content{0}->sectionTitle) && isset($page->content{0}->sectionContent))
 <section class="uk-section-default uk-padding-small-top uk-padding-small-bottom">
     <div class="uk-container uk-padding uk-padding-remove-horizontal">
-            <div class="section-title">
+        <div uk-grid>
+            <div class="uk-width-1-1">
+                <div class="section-title">
                     <h3 class="heading-tertiary">{!!$page->content{0}->sectionTitle!!}</h3>
                     <span class="divide-line"></span>
                 </div>
-        <p class="uk-h4 uk-text-center">
-            @if (isset($page->content{0}->sectionContent))
-                {!!$page->content{0}->sectionContent!!}
-            @endisset
-        </p>
+            </div>
+            <div class="uk-width-1-5 uk-margin-remove-top"></div>
+            <div class="uk-width-3-5 uk-margin-remove-top">
+                <p class="uk-text-center">
+                    {!!$page->content{0}->sectionContent!!}
+                </p>
+            </div>
+            <div class="uk-width-1-5 uk-margin-remove-top"></div>
+        </div>
+    </div>
+</section>
+@endif
+<section class="uk-section-default uk-padding-small-top uk-padding-small-bottom">
+    <div class="uk-container uk-padding uk-padding-remove-horizontal">
         <div uk-grid>
             <div class="uk-width-1-2">
                 <h3 class="uk-h3">1. Source of the raw material</h3>
