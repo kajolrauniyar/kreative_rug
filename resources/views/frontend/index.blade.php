b8c20fbabc4249c0339086eb03e174bbf6b4b952
Kajol2608 <rauniyarkajol2608@gmail.com>
2019-05-09 11:51:25 +0545

    form

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,75 +1,76 @@
-@extends('layouts.frontend') 
+@extends('layouts.frontend')
 @section('content')
 <header class="header" data-src="{{asset($home->banner)}}" uk-img>
-     <div class="uk-position-center-center header--text">
+    <div class="uk-position-center-center header--text">
         <h1 class="uk-margin-remove">{{$home->heading}}</h1>
         <h2>{{$home->subheading}}</h2>
-     </div>
+    </div>
 </header>
 <main>
-        <section class="section-intro">
-                <div class="section-title">
-                    <h2 class="heading-secondary">{{$home->section1_title}}</h2>
-                    <span class="divide-line"></span>
-                </div>
-                <div uk-grid>
-                        <div class="uk-width-1-2"></div>
-                        <div class="uk-width-1-2"></div>
+    <section class="section-intro">
+        <div class="section-title">
+            <h2 class="heading-secondary">{{$home->section1_title}}</h2>
+            <span class="divide-line"></span>
+        </div>
+        <div uk-grid>
+            <div class="uk-width-1-1">
+                <p class="paragraph">
+                    {{$home->section1_content}}
+                </p>
+            </div>
+        </div>
+        <div class="uk-grid-medium  uk-text-center uk-grid-match" uk-grid>
+            <div class="uk-width-1-1">
+                <!--840x360-->
+                <img src="{{$home->section1_image}}" alt="{{config('app.name')}}">
+            </div>
+        </div>
+    </section>
+    <section class="value-grid">
+        <div class="section-title">
+            <h2 class="heading-secondary">Essense of Kreative Rugs</h2>
+            <span class="divide-line"></span>
+        </div>
+        <div class="uk-child-width-1-3@m uk-grid-match uk-grid-small" uk-grid>
+            <div>
+                <div class="value-item">
+                    <div class="uk-card-media-top">
+                        <img src="https://source.unsplash.com/450x300/?vinyl" alt="">
                     </div>
-                <div class="uk-grid-medium  uk-text-center uk-grid-match" uk-grid>
-                    <div class="uk-width-3-5">
-                        <p class="paragraph">
-                            {{$home->section1_content}}
+                    <div class="uk-card-body uk-paddin-small">
+                        <h3 class="uk-card-title">High Quality Raw Materials</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
                         </p>
                     </div>
-                    <div>
-                        <!--840x360-->
-                        <img src="{{$home->section1_image}}" alt="{{config('app.name')}}">
+                </div>
+            </div>
+            <div>
+                <div class="value-item">
+                    <div class="uk-card-body uk-paddin-small">
+                        <h3 class="uk-card-title">Traditionally Hand Knotted in Nepal</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
+                        </p>
+                    </div>
+                    <div class="uk-card-media-bottom">
+                        <img src="https://source.unsplash.com/450x300/?cup" alt="">
                     </div>
                 </div>
-            </section>
-<section class="value-grid">
-        <div class="section-title">
-                <h2 class="heading-secondary">Essense of Kreative Rugs</h2>
-                <span class="divide-line"></span>
             </div>
-    <div class="uk-child-width-1-3@m uk-grid-match uk-grid-small" uk-grid>
-        <div>
-            <div class="value-item">
-                <div class="uk-card-media-top">
-                    <img src="https://source.unsplash.com/450x300/?vinyl" alt="">
-                </div>
-                <div class="uk-card-body uk-paddin-small">
-                    <h3 class="uk-card-title">High Quality Raw Materials</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+            <div>
+                <div class="value-item">
+                    <div class="uk-card-media-top">
+                        <img src="https://source.unsplash.com/450x300/?vase" alt="">
+                    </div>
+                    <div class="uk-card-body uk-paddin-small">
+                        <h3 class="uk-card-title">Environment Friendly</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
-        <div>
-            <div class="value-item">
-                <div class="uk-card-body uk-paddin-small">
-                    <h3 class="uk-card-title">Traditionally Hand Knotted in Nepal</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
-                </div>
-                <div class="uk-card-media-bottom">
-                    <img src="https://source.unsplash.com/450x300/?cup" alt="">
-                </div>
-            </div>
-        </div>
-        <div>
-            <div class="value-item">
-                <div class="uk-card-media-top">
-                    <img src="https://source.unsplash.com/450x300/?vase" alt="">
-                </div>
-                <div class="uk-card-body uk-paddin-small">
-                    <h3 class="uk-card-title">Environment Friendly</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
-                </div>
-            </div>
-        </div>        
-    </div>
-</section>
-<section class="section-featured">
+    </section>
+    <section class="section-featured">
         <div class="section-title">
             <h2 class="heading-secondary">{{$home->section2_title}}</h2>
             <span class="divide-line"></span>
@@ -92,13 +93,15 @@
                 @endforeach
             </ul>
 
-            <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
-            <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
+            <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous
+                uk-slider-item="previous"></a>
+            <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next
+                uk-slider-item="next"></a>
 
         </div>
     </section>
 
-<!--
+
 <section class="value-grid">
     <div class="uk-child-width-1-4@m uk-grid-match uk-grid-small" uk-grid>
         <div>
@@ -149,23 +152,23 @@
             </div>
         </div>
     </div>
-</section> --> 
+</section> 
     {{-- <section class="section-intro">
         <div class="section-title">
             <h2 class="heading-secondary">{{$home->section1_title}}</h2>
-            <span class="divide-line"></span>
+    <span class="divide-line"></span>
+    </div>
+    <div class="uk-grid-medium  uk-text-center uk-grid-match" uk-grid>
+        <div class="uk-width-1-2@m uk-width-1-2@l uk-width-1-1@s">
+            <p class="paragraph">
+                {{$home->section1_content}}
+            </p>
         </div>
-        <div class="uk-grid-medium  uk-text-center uk-grid-match" uk-grid>
-            <div class="uk-width-1-2@m uk-width-1-2@l uk-width-1-1@s">
-                <p class="paragraph">
-                    {{$home->section1_content}}
-                </p>
-            </div>
-            <div class="uk-width-1-2@m uk-width-1-2@l uk-width-1-1@s">
-                <!--840x360-->
-                <img src="{{$home->section1_image}}" alt="{{config('app.name')}}">
-            </div>
+        <div class="uk-width-1-2@m uk-width-1-2@l uk-width-1-1@s">
+            <!--840x360-->
+            <img src="{{$home->section1_image}}" alt="{{config('app.name')}}">
         </div>
+    </div>
     </section>
 
     <section class="section-featured">
@@ -191,8 +194,10 @@
                 @endforeach
             </ul>
 
-            <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
-            <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
+            <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous
+                uk-slider-item="previous"></a>
+            <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next
+                uk-slider-item="next"></a>
 
         </div>
     </section>
