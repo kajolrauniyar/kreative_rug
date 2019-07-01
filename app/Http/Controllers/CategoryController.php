3c41a1c07c6f4f99cca040d60bde75ba048526b8
Kajol2608 <rauniyarkajol2608@gmail.com>
2019-07-01 15:19:13 +0545

    slider

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -136,7 +136,7 @@ class CategoryController extends Controller
             $UploadImage = new UploadImage;
             $path = $UploadImage->uploadSingle($this->path,$media->path,1024,512);
             $thumb = $UploadImage->uploadSingle($this->thumb,$media->path,320,480);
-            $nav = $UploadImage->cropUpload($this->nav,$media->path,400,600
+            $nav = $UploadImage->cropUpload($this->nav,$media->path,400,600);
            $category->path = $path;
            $category->thumb = $thumb;
            $category->nav = $nav;
